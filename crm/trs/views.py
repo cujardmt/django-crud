e433ac0ce2d7d014431f8c9b8bf5fab42946e4a8
Moises Cujardo <cujardo.moises@gmail.com>
2024-04-22 20:49:29 +0800

    base travel request

--- crm/trs/views.py
+++ crm/trs/views.py
@@ -8,18 +8,22 @@ from django.contrib import messages
 def home(request):
     return render(request, 'trs/index.html')
 
+# create a travel request
 @login_required()
 def createTravelRequest(request):
     return render(request, 'trs/create-travel-request.html')
 
+# read single travel request
 @login_required()
 def readTravelRequest(request):
     return render(request, 'trs/read-travel-request.html')
 
+# update travel requests
 @login_required()
 def updateTravelRequest(request):
     return render(request, 'trs/update-travel-request.html')
 
+# delete
 @login_required()
 def deleteTravelRequest(request):
     pass
