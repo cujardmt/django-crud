d1b716bca6166645a26abbeea5ce4f04b41eb7aa
Moises Cujardo <cujardo.moises@gmail.com>
2024-04-22 20:46:29 +0800

    init version

--- crm/trs/views.py
+++ crm/trs/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.models import auth
-from django.contrib.auth import logout
-from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 
 # - Home Page
+@login_required()
 def home(request):
     return render(request, 'trs/index.html')
 
-
+@login_required()
 def createTravelRequest(request):
     return render(request, 'trs/create-travel-request.html')
 
+@login_required()
 def readTravelRequest(request):
     return render(request, 'trs/read-travel-request.html')
 
+@login_required()
 def updateTravelRequest(request):
     return render(request, 'trs/update-travel-request.html')
 
+@login_required()
 def deleteTravelRequest(request):
     pass
